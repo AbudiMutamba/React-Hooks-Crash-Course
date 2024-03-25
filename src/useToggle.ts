1b7329b0d051396e78f7154f01056b305168dcef
AbudiMutamba <mutambaabudi@gmail.com>
2024-03-25 18:34:17 +0300

    toggle app

---
+++ src/useToggle.ts
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const useToggle = () => {
+
+    const [value, setValue] = useState(false);
+
+    const toggle = () => {
+        setValue(currentValue =>! currentValue);
+    };
+
+    return [
+        value, toggle
+    ] as const ;
+};
+
+export default useToggle;
